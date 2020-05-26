69ad752fb8bcb553d7f56cf8b46b671433f419f2
satyam <satyam06052000@gmail.com>
2020-05-26 19:19:21 +0530

    My first commit

---
+++ poll_project/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/satyam/PycharmProjects/POLL_PROJECT_1/poll_project/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
